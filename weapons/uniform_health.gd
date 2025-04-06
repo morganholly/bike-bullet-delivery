ab91673b614d8a0e6475d630c11a639ff400c0b3
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-04-05 20:12:33 -0400

    start on health system

---
+++ weapons/uniform_health.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+@export_group("Health")
+@export var max_health: float = 0
+@export var health_regen: float = 0
+
+@export_group("Armor")
+@export var max_armor: float = 0
+@export var armor_regen: float = 0
+
+@export_group("Energy Armor")
+@export var max_energy_armor: float = 0
+@export var max_energy: float = 0
+@export var energy_armor_recharge: float = 0
+@export var energy_recharge: float = 0
