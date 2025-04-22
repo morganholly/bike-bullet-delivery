6df1b3442c51d96a5ba7f3211fe9a25bc1b8f898
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-04-22 18:43:50 -0400

    start on gun impl

--- weapons/gun.gd
+++ weapons/gun.gd
@@ -1,13 +1,34 @@
 extends RigidBody3D
 
-@export var gun_mesh: Mesh
+@export var gun_mesh: PackedScene
 @export var gun_stats: GunStats
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var instance = gun_mesh.instantiate()
+	add_child(instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func shoot(from: Vector3, to: Vector3, ammo_pool: Node) -> void:
+	if ray_cast_3d.is_colliding():
+		if ray_cast_3d.get_collider().is_in_group(&"Damageable"):
+			var parent_object = ray_cast_3d.get_collider()
+			var health_manager: Node
+			for child in parent_object.get_children() :
+				if child.is_in_group(&"HealthManager") :
+					health_manager = child
+					break
+			if not gun_stats.infinite_ammo:
+				if ammo_pool.ammo_dict[gun_stats.bullet_id] > 0:
+					ammo_pool.ammo_dict[gun_stats.bullet_id] -= 1
+					health_manager.damage(gun_stats.shot_damage)
+			else:
+				health_manager.damage(gun_stats.shot_damage)
--- weapons/gun_stats.gd
+++ weapons/gun_stats.gd
@@ -3,6 +3,17 @@ class_name GunStats
 
 enum HitType {
 	Hitscan
+	# i'd like to add an area mode that takes into account travel time, but not needed for the jam
 }
 @export var hit_type: HitType = HitType.Hitscan
-@export var bullet_speed: float = 1000
+@export var bullet_speed: float = 350
+enum BulletType {
+	_9MM,
+	_223_556,
+	_22LR,
+	_12Gauge,
+	_308_762x51
+}
+@export var bullet_id: BulletType = BulletType._9MM
+@export var shot_damage: float = 1.0
+@export var infinite_ammo: bool = false
