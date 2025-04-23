84cf51515bbf678f1c427fd24c93275a6b5f8d39
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-04-23 15:57:52 -0400

    fix error with looking at in mesh face camera script

--- npcs/mesh_face_camera.gd
+++ npcs/mesh_face_camera.gd
@@ -2,5 +2,5 @@ extends MeshInstance3D
 
 
 func _process(delta: float) -> void:
-	var cam_pos = get_viewport().get_camera_3d().global_transform.origin
-	self.global_basis = self.global_basis.looking_at(cam_pos)
+	var cam_pos = get_viewport().get_camera_3d().global_position
+	self.global_basis = self.global_basis.looking_at(cam_pos, Vector3.UP + self.global_position)
