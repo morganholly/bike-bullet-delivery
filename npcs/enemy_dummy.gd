abb335d099e5e1be2aa8f7b7f196f3c8a031b888
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-04-24 15:18:00 -0400

    update debug text for dummy health regen

--- npcs/enemy_dummy.gd
+++ npcs/enemy_dummy.gd
@@ -9,10 +9,11 @@ func dead() -> void:
 	mesh_main.mesh.surface_get_material(0).albedo_color = Color("ff4221")
 
 func damaged() -> void:
-	print("damaged")
+	mesh_main.mesh.surface_get_material(0).albedo_color = lerp(Color("1232a0"), Color("564231"), 1 - uniform_health.current_health / uniform_health.max_health)
+	#print("damaged")
 
 func hit() -> void:
-	print("hit")
+	#print("hit")
 	health_text.mesh.text = str(roundf(uniform_health.current_health * 10) / 10) + " / " + str(uniform_health.max_health)
 	armor_text.mesh.text = str(roundf(uniform_health.current_armor * 10) / 10) + " / " + str(uniform_health.max_armor)
 
@@ -28,3 +29,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	hit()
