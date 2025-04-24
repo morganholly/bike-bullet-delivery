ee892db6487a6ae5ec9b861b46e9e40b93c828c7
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-04-24 16:18:41 -0400

    allow setting ammo pickup to not take while holding

--- weapons/bullet_pickup.gd
+++ weapons/bullet_pickup.gd
@@ -3,10 +3,13 @@ extends Area3D
 
 @export var free_ammo: Dictionary[GunStats.BulletType, int]
 @export var full_mags: Dictionary[GunStats.BulletType, int]
-
+@export var take_while_holding: bool = true
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group(&"Player"):
+		if not take_while_holding:
+			if self.get_parent().is_in_group(&"IsHeld"):
+				return
 		for bt in GunStats.BulletType.values():
 			body.hold_container.ammo_pool.free_ammo[bt] = free_ammo.get_or_add(bt, 0) + body.hold_container.ammo_pool.free_ammo.get_or_add(bt, 0)
 			self.free_ammo[bt] = 0
