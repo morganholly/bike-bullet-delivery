0acaa5319a0a8688abe4cf718ef9ae9ba3e30cdc
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-05-05 18:32:30 -0400

    add thing spawner maintains specific number of things can pick from multiple scenes checks that it doesn't spawn too high checks overlap between things (doesn't check wall overlap yet) can spawn in editor and bring placements into the game to improve initial game load

--- utils/thing_spawner.gd
+++ utils/thing_spawner.gd
@@ -46,11 +46,14 @@ var current_num_items: int = 0
 var counter_use_could_fit: int = 0
 
 @export_group("Checking")
+@export_range(1, 6000, 1) var frames_per_spawn_in_game: int = 1
+var frame_counter: int = 0
 @export_flags_3d_physics var collision_hit: int = 1
 @export_flags_3d_physics var collision_place: int = 1
 @export var desired_normal: Vector3 = Vector3.UP
 @export_range(-1, 1, 0.01) var normal_dot_threshold: float = -1
-@export var height_up_check: float = 200
+@export var height_up_raycast: float = 200
+@export var height_up_place: float = 0.1
 enum CheckMode {
 	PlainRandom,
 	PoissonDart,
@@ -77,22 +80,22 @@ var poisson_dist_sq: float = 4
 @export_group("Shape Fitting")
 @export var check_shape_fit: bool = true
 @export var shape_fit: Shape3D
+var check_shapes: Array[Dictionary]
 
 @export_group("Player")
-## ignored when running in editor
+@export var editor_no_respawn_near_player: bool = true
+## when running
 @export var no_respawn_near_player: bool = true
 var respawn_player_dist_sq: float = 50 * 50
 @export var respawn_player_distance: float = 50:
 	set(value):
 		respawn_player_distance = value
 		respawn_player_dist_sq = value * value
-var player_position: Vector3
 
 @export_group("")
 ## turn off if you generate ahead of time and tweak placement of indestructable or immovable objects
 @export var continually_check: bool = true
 @export var place_in_editor: bool = false
-## removes nodes without removing grid nodes
 
 func remove():
 	things_list.clear()
@@ -100,9 +103,11 @@ func remove():
 		child.free()
 	_ready()
 
+## removes nodes without removing grid nodes
 @export_tool_button("remove") var remove_all = remove
 
-@export var things_list: Array[Node3D]
+#@export var things_list: Array[Node3D]
+@export var things_list: Array[Dictionary]
 
 func make_grid(min_x: float, width_x: float, min_z: float, width_z: float):
 	var bins_per_axis: int = int(ceil(sqrt(num_items) / sqrt(poisson_grid_object_count)))
@@ -121,9 +126,48 @@ func make_grid(min_x: float, width_x: float, min_z: float, width_z: float):
 			x_row.add_child(z_grid)
 		things.add_child(x_row)
 
+func extract_shapes(node3d: Node3D) -> Dictionary[String, Array]:
+	var shapes: Array[Shape3D]
+	var masks: Array[int]
+	var transforms: Array[Transform3D]
+	for c1 in node3d.get_children():
+		if c1.is_in_group(&"SpawnerExclusionArea"):
+			#shapes.append()
+			for c2 in c1.get_children():
+				if c2 is CollisionShape3D:
+					shapes.append(c2.shape)
+					masks.append(c1.collision_mask)
+					transforms.append(c2.global_transform)
+	var result: Dictionary[String, Array]
+	result["shapes"] = shapes
+	result["masks"] = masks
+	result["transforms"] = transforms
+	return result
+
+func remove_shapes(node3d: Node3D):
+	for c1 in node3d.get_children():
+		if c1.is_in_group(&"SpawnerExclusionAreaRemovable"):
+			c1.free()
+
+func check_shape_set(shape_dict: Dictionary[String, Array]) -> bool:
+	for i in range(0, len(shape_dict.shapes)):
+		var query = PhysicsShapeQueryParameters3D.new()
+		query.collision_mask = shape_dict.masks[i]
+		query.shape = shape_dict.shapes[i]
+		query.transform = shape_dict.transforms[i]
+		var result = get_world_3d().direct_space_state.intersect_shape(query, 1)
+		if len(result) > 0:
+			return false
+	return true
+
 func _ready():
+	if not Engine.is_editor_hint():
+		$"Valid Area".queue_free()
 	if shape_fit == null:
 		check_shape_fit = false
+	if check_shape_fit:
+		for i in range(0, len(scenes)):
+			check_shapes[i] = extract_shapes(scenes[i].instantiate())
 	
 	var avg_y: float = (max_y + min_y) / 2
 	var width_y: float = abs(max_y - min_y)
@@ -143,11 +187,25 @@ func _ready():
 	
 	if check_mode == CheckMode.PoissonGridDart:
 		make_grid(min(min_x, max_x), width_x, min(min_z, max_z), width_z)
+	
+	print(things.get_child_count(), " ", len(things_list))
+	if things.get_child_count() < len(things_list):
+		for child in things.get_children():
+			#child.free()
+			things.remove_child(child)
+		for thing in things_list:
+			var instance = thing.node.instantiate()
+			instance.position = thing.position
+			if instance is RigidBody3D:
+				instance.sleeping = true
+				#instance.set_deferred("sleeping", true)
+			remove_shapes(instance)
+			things.add_child(instance)
+	print(things.get_child_count(), " ", len(things_list))
 
 func test_point(from: Vector3, to: Vector3) -> Array:
 	var space_state = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(from, to,
-		collision_hit, [self])
+	var query = PhysicsRayQueryParameters3D.create(from, to, collision_hit)
 	var result = space_state.intersect_ray(query)
 	if not result:
 		return [false, Vector3.ZERO, Vector3.ZERO]
@@ -164,6 +222,8 @@ func _physics_process(delta: float) -> void:
 	var place: bool = false
 	var check_player_dist: bool = false
 	if Engine.is_editor_hint():
+		if editor_no_respawn_near_player:
+			check_player_dist = true
 		if place_in_editor:
 			place = true
 		if recalc_area:
@@ -185,73 +245,95 @@ func _physics_process(delta: float) -> void:
 		if no_respawn_near_player:
 			check_player_dist = true
 	if place:
-		current_num_items = things.get_child_count()
-		var diff = (num_items if counter_use_could_fit <= 0 else could_fit_items) - current_num_items
-		counter_use_could_fit = max(0, counter_use_could_fit - 1)
-		if diff > 0:
-			var new_packed_scene = scenes[randi_range(0, len(scenes) - 1)]
-			match check_mode:
-				CheckMode.PlainRandom:
-					var new_pos: Vector3
-					var found_pos: bool = true
-					for i in range(0, poisson_tries):
-						new_pos = Vector3(randf_range(min_x, max_x), 0, randf_range(min_z, max_z)) + self.global_position
-						found_pos = true
-						if check_player_dist:
-							if (new_pos + self.global_position).distance_squared_to(player_position) > respawn_player_dist_sq:
-								found_pos = false
-						if found_pos:
-							var new_scene = new_packed_scene.instantiate()
-							var from_pos = new_pos + self.global_position + Vector3(0, max_y + height_up_check, 0)
-							new_pos += self.global_position + Vector3(0, min_y, 0)
-							var result = test_point(from_pos, new_pos)
-							if result[0]:
-								new_scene.position = result[1] + Vector3(0, 50, 0)
-								#Basis.looking_at()
-								things_list.append(new_scene)
-								things.add_child(new_scene)
-								break
-							else:
-								found_pos = false
-				CheckMode.PoissonDart:
-					var new_pos: Vector3
-					var found_pos: bool = true
-					for i in range(0, poisson_tries):
-						new_pos = Vector3(randf_range(min_x, max_x), 0, randf_range(min_z, max_z)) + self.global_position
-						found_pos = true
-						var new_global_pos = (new_pos + self.global_position)
-						if check_player_dist:
-							if new_global_pos.distance_squared_to(player_position) > respawn_player_dist_sq:
-								found_pos = false
-						if found_pos:
-							for ch in things.get_children():
-								if new_global_pos.distance_squared_to(ch.position) > poisson_dist_sq:
+		frame_counter -= 1
+		if frame_counter <= 0 or Engine.is_editor_hint():
+			frame_counter = frames_per_spawn_in_game
+			current_num_items = things.get_child_count()
+			var diff = (num_items if counter_use_could_fit <= 0 else could_fit_items) - current_num_items
+			counter_use_could_fit = max(0, counter_use_could_fit - 1)
+			#print(diff)
+			if diff > 0:
+				match check_mode:
+					CheckMode.PlainRandom:
+						var new_pos: Vector3
+						var found_pos: bool = true
+						for i in range(0, poisson_tries):
+							new_pos = Vector3(randf_range(min_x, max_x), 0, randf_range(min_z, max_z)) + self.global_position
+							found_pos = true
+							var scene_idx: int = randi_range(0, len(scenes) - 1)
+							if check_player_dist:
+								var check_pos = (new_pos + self.global_position) * Vector3(1, 0, 1)
+								var check_player = GlobalPlayerData.player_position * Vector3(1, 0, 1)
+								if check_pos.distance_squared_to(check_player) < respawn_player_dist_sq:
 									found_pos = false
+							if check_shape_fit:
+								if not check_shape_set(check_shapes[scene_idx]):
+									found_pos = false
+							if found_pos:
+								var new_packed_scene = scenes[scene_idx]
+								var from_pos = new_pos + self.global_position + Vector3(0, max_y + height_up_raycast, 0)
+								new_pos += self.global_position + Vector3(0, min_y, 0)
+								var result = test_point(from_pos, new_pos)
+								if result[0]:
+									#var array_scene = new_packed_scene.instantiate()
+									#array_scene.position = result[1]# + Vector3(0, 50, 0)
+									#Basis.looking_at()
+									if Engine.is_editor_hint():
+										var inst_info: Dictionary
+										inst_info["node"] = new_packed_scene
+										inst_info["position"] = result[1] + Vector3(0, height_up_place, 0)
+										things_list.append(inst_info)
+									var node_scene = new_packed_scene.instantiate()
+									node_scene.position = result[1] + Vector3(0, height_up_place, 0)
+									remove_shapes(node_scene)
+									things.add_child(node_scene)
 									break
-						if found_pos:
-							var new_scene = new_packed_scene.instantiate()
-							var from_pos = new_global_pos + Vector3(0, max_y + height_up_check, 0)
-							new_global_pos += Vector3(0, min_y, 0)
-							var result = test_point(from_pos, new_global_pos)
-							if result[0]:
-								new_scene.position = result[1]
-								#Basis.looking_at()
-								things.add_child(new_scene)
-								break
-							else:
-								found_pos = false
-				CheckMode.PoissonGridDart:
+								else:
+									found_pos = false
+					CheckMode.PoissonDart:
+						var new_pos: Vector3
+						var found_pos: bool = true
+						for i in range(0, poisson_tries):
+							new_pos = Vector3(randf_range(min_x, max_x), 0, randf_range(min_z, max_z)) + self.global_position
+							found_pos = true
+							var new_global_pos = (new_pos + self.global_position)
+							if check_player_dist:
+								if new_global_pos.distance_squared_to(GlobalPlayerData.player_position) > respawn_player_dist_sq:
+									found_pos = false
+							if found_pos:
+								for ch in things.get_children():
+									if new_global_pos.distance_squared_to(ch.position) > poisson_dist_sq:
+										found_pos = false
+										break
+							if found_pos:
+								var new_packed_scene = scenes[randi_range(0, len(scenes) - 1)]
+								var new_scene = new_packed_scene.instantiate()
+								var from_pos = new_global_pos + Vector3(0, max_y + height_up_raycast, 0)
+								new_global_pos += Vector3(0, min_y, 0)
+								var result = test_point(from_pos, new_global_pos)
+								if result[0]:
+									new_scene.position = result[1]
+									#Basis.looking_at()
+									things.add_child(new_scene)
+									break
+								else:
+									found_pos = false
+					CheckMode.PoissonGridDart:
+						pass
+						# when spawning items, place them into the appropriate grid node
+						# filling should check instead quantity per grid node
+				for i in diff:
 					pass
-					# when spawning items, place them into the appropriate grid node
-					# filling should check instead quantity per grid node
-			for i in diff:
+			elif diff < 0:
 				pass
-		elif diff < 0:
-			pass
-			# implement optional deleting
-		if diff > 0:
-			var new_diff = num_items - things.get_child_count()
-			if new_diff / diff > 0.6:
-				could_fit_items = things.get_child_count()
-				# wait 10 seconds to try again in full
-				counter_use_could_fit = 600
+				# implement optional deleting
+			if diff == 1:
+				print("last placed")
+			if Engine.is_editor_hint() and diff == 0:
+				place_in_editor = false
+			if diff > 0:
+				var new_diff = num_items - things.get_child_count()
+				if new_diff / diff > 0.6:
+					could_fit_items = things.get_child_count()
+					# wait 10 seconds to try again in full
+					counter_use_could_fit = 600
